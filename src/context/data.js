d45d986178aaeee7b40fdef088a6aff777d55092
Sinzie07 <sinzie@hotmail.com>
2023-06-12 02:33:44 +0700

    ubah nama

--- src/context/data.js
+++ src/context/data.js
@@ -139,7 +139,7 @@ export const alternatives = [
   {
     id: "compas",
     title: "Computer & Society",
-    image: "blockchain.jpg",
+    image: "computersociety1.jpg",
     difficulty: 3,
     href: "/detailsociety",
     desc: "The rapid development of ICT has changed many activities in our lives. For example, in learning, playing, working, communication and doing business. Moreover, now social media has also been widely used in various ways. In addition to having a positive impact, the development of ICT also has a negative impact needs to be understood so that the community using ICT can minimize its impact. To that end, the Indonesian government has drawn up various laws, including the Law of Information and Electronic Transactions to regulate the use of ICT in society. As an ICT professional, it is not enough just to master the technology but also to understand the code ethics to prevent the misuse of ICT, both for personal interests and the interests of other parties.",
